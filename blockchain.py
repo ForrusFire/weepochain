5138a6592e3d0cce234ca2358c7df8efa9875a49
Sam Chun <sc.wjstoa@gmail.com>
2021-03-23 18:13:08 -0700

    added wallet and transaction verification

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,17 @@
 import hashlib
 import json
 from time import time
+from collections import OrderedDict
 
 from itertools import permutations
 import random
 
-# starting methods below taken from https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
+import binascii
+import Crypto.Random
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
+
 
 class Blockchain():
     def __init__(self):
@@ -15,13 +21,21 @@ class Blockchain():
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
 
+        # (TODO) Add a node list with node ids on the network;
+        # will also probably need methods that add nodes to the network
+
     def print_blockchain(self):
+        """
+        Prints the blockchain
+        """
         for block in self.chain:
             print(block)
             print()
 
     def new_block(self, proof, previous_hash=None):
-        # creates a new block        
+        """
+        Creates a new block, appends it to the chain, and returns it
+        """   
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time(),
@@ -30,6 +44,8 @@ class Blockchain():
             'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
 
+        # (TODO): Add mining reward
+
         # Reset the current list of transactions
         self.current_transactions = []
 
@@ -37,21 +53,37 @@ class Blockchain():
         self.chain.append(block)
         return block
     
-    def new_transaction(self, sender, recipient, amount):
-        # adds a transaction to the current list
-        self.current_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount,
-        })
+    def new_transaction(self, sender, sender_private_key, recipient, amount):
+        """
+        Adds a transaction to the current list, 
+        and returns the index of the block the transaction is in
+        """
+        transaction = Transaction(sender, sender_private_key, recipient, amount)
+        
+        verification = self.verify_transaction(sender, transaction.sign_transaction(), transaction.to_dict())
+        # returns index of block this transaction is in, or -1 if the transaction verification failed
+        print(verification)
+        if verification:
+            self.current_transactions.append(transaction.to_dict())
+            return len(self.chain) + 1
+        else:
+            return -1
 
-        # returns index of block this transaction is in
-        return self.last_block['index'] + 1
+    def verify_transaction(self, sender, signature, transaction):
+        """
+        Check that the provided signature corresponds to transaction
+        signed by the public key
+        """
+        public_key = RSA.importKey(binascii.unhexlify(sender))
+        verifier = PKCS1_v1_5.new(public_key)
+        h = SHA.new(str(transaction).encode('utf8'))
+        return verifier.verify(h, binascii.unhexlify(signature))
     
     @staticmethod
     def hash(block):
-        # hashes a block
-
+        """
+        Hashes a block and returns the hash
+        """
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode('utf-8')
 
@@ -67,8 +99,7 @@ class Blockchain():
         Simple Proof of Work Algorithm:
          - Find a number p' such that hash(pp') contains leading 4 zeroes
          - p is the previous proof, and p' is the new proof
-        :param last_proof: <int>
-        :return: <int>
+        Returns the new proof
         """
 
         proof = 0
@@ -81,26 +112,80 @@ class Blockchain():
     def valid_proof(last_proof, proof):
         """
         Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
-        :param last_proof: <int> Previous Proof
-        :param proof: <int> Current Proof
-        :return: <bool> True if correct, False if not.
+        Returns a bool that validates the proof
         """
 
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+    
+    @staticmethod
+    def valid_chain(self, chain):
+        """
+        (TODO): Check if a blockchain is valid
+        """
+    
+    def resolve_conflicts(self):
+        """
+        (TODO): Resolve conflicts between blockchain nodes
+        by replacing our chain with the longest one in the network.
+        """
 
+
+# (TODO): Put the Transaction class in a separate client file with Flask
+class Transaction:
+    def __init__(self, sender, sender_private_key, recipient, amount):
+        self.sender = sender
+        self.sender_private_key = sender_private_key
+        self.recipient = recipient
+        self.amount = amount
+
+    def __getattr__(self, attr):
+        return self.data[attr]
+
+    def to_dict(self):
+        """
+        Returns the transaction as an ordered dictionary
+        """
+        return OrderedDict({'sender': self.sender,
+                            'recipient': self.recipient,
+                            'amount': self.amount})
+
+    def sign_transaction(self):
+        """
+        Signs the transaction with the sender's private key
+        """
+        private_key = RSA.importKey(binascii.unhexlify(self.sender_private_key))
+        signer = PKCS1_v1_5.new(private_key)
+        h = SHA.new(str(self.to_dict()).encode('utf8'))
+        return binascii.hexlify(signer.sign(h)).decode('ascii')
+
+def new_wallet():
+    """
+    Creates a new wallet with a public and private key
+    """
+    random_gen = Crypto.Random.new().read
+    private_key = RSA.generate(2048, random_gen)
+    public_key = private_key.publickey()
+    keys = {
+        'private_key': binascii.hexlify(private_key.exportKey(format='DER')).decode('ascii'),
+        'public_key': binascii.hexlify(public_key.exportKey(format='DER')).decode('ascii')
+    }
+    return keys
 
 
 
 if __name__ == "__main__":
-    # simple testing
+    # create blockchain instance
     blockchain = Blockchain()
-
     random.seed(0)
 
+    # establish users and users' wallets
     users = ["Alice", "Bob", "Carmen", "Dan", "Edgar"]
-    perm = list(permutations(users, 2))
+    users_perm = list(permutations(users, 2))
+    wallets = {user: new_wallet() for user in users}
+
+    # parameters
     max_amount = 100
 
     num_transactions = 5
@@ -108,11 +193,11 @@ if __name__ == "__main__":
 
     for _ in range(num_blocks-1):
         # adds five new transactions from the user list
-        for i in range(5):
-            transaction = perm[random.randint(1, len(perm)-1)]
+        for _ in range(num_transactions):
+            sender, recipient = users_perm[random.randint(1, len(users_perm)-1)]
             amount = random.randint(1, max_amount)
 
-            blockchain.new_transaction(transaction[0], transaction[1], amount)
+            blockchain.new_transaction(wallets[sender]['public_key'], wallets[sender]['private_key'], wallets[recipient]['public_key'], amount)
         
         # mine new block
         last_proof = blockchain.last_block['proof']
