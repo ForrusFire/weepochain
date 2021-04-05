af5f7f9e143b3546d8a725a98fd6ccd06bc96983
Sam Chun <sc.wjstoa@gmail.com>
2021-04-04 22:06:50 -0700

    blockchain visualizer update

--- blockchain/static/client.js
+++ blockchain/static/client.js
@@ -35,46 +35,96 @@ $(document).ready(function(){
     });
 
 
-    // Load transactions on the chain
+    // Load blocks on the chain
     $.ajax({
         url: "/blocks",
         type: "GET",
         success: function(response){
-            // Generate transactions table
-            var transactions = [];
-            count = 1;
+            var count = 0;
+            var row = $('<div>', {class: "row"});
 
-            for (i = 0; i < response['length']; i++){
-                for (j = 0; j < response['chain'][i]['transactions'].length; j++){
-                    // Date format
+            // Add four blocks to each row, starting from the latest block
+            for (i = response['length']-1; i >= 0; i--){
+                // Create block html elements
+                var block = $('<div>', {class: "col-lg-3"});
+                var card = $('<div>', {class: "card"})
+                var card_body = $('<div>', {class: "card-body"});
+                var card_text = $('<div>', {class: "card-text"});
+                
+
+                // Don't add transactions to the genesis block
+                if (i != 0) {
+                    // Append card header with index and previous hash
+                    var block_index = $('<h4>', {text: "Block #" + response['chain'][i]['index']});
+                    var prev_hash_str = response['chain'][i]['previous_hash'];
+                    var prev_hash = $('<strong>', {text: prev_hash_str.substring(0, 5) + "..." + prev_hash_str.substring(prev_hash_str.length - 5, prev_hash_str.length)});
+                    var prev_hash_elem = $('<p>', {text: "Previous Hash: "});
+                    var hr = $('<hr>');
+                    prev_hash_elem.append(prev_hash);
+                    card_text.append(block_index, prev_hash_elem, hr);
+
+                    // Add the block's transactions
+                    var transaction;
+                    for (j = 0; j < response['chain'][i]['transactions'].length; j++){
+                        var recipient_str = response['chain'][i]['transactions'][j]['recipient'];
+                        var recipient = $('<strong>', {text: recipient_str.substring(0, 6) + "..." + recipient_str.substring(recipient_str.length - 6, recipient_str.length)});
+                        var sender_str = response['chain'][i]['transactions'][j]['sender'];
+                        var sender = $('<strong>', {text: sender_str.substring(0, 6) + "..." + sender_str.substring(sender_str.length - 6, sender_str.length)});
+
+                        // Change the transaction's background color depending on the type of transaction
+                        if (response['chain'][i]['transactions'][j]['sender'] == 'blockchain'){
+                            // Reward transaction
+                            transaction = $('<div>', {class: "alert alert-warning", text: response['chain'][i]['transactions'][j]['amount'] + " to "});
+                            transaction.append(recipient);
+                        } else {
+                            // Standard transaction
+                            transaction = $('<div>', {class: "alert alert-success"});
+                            
+                            var recipient_transaction = $('<div>', {text: response['chain'][i]['transactions'][j]['amount'] + " to "});
+                            recipient_transaction.append(recipient);
+                            transaction.append(recipient_transaction);
+
+                            var sender_transaction = $('<div>', {text: response['chain'][i]['transactions'][j]['amount'] + " from "});
+                            sender_transaction.append(sender);
+                            var br = $('<br>');
+                            transaction.append(br, sender_transaction);
+                        };
+
+                        // Append transaction to card
+                        card_text.append(transaction);
+                    };
+
+                    // Format date and time, and append it to the card
                     var options = {year: "numeric", month: "short", day: "numeric", hour: "2-digit", minute: "2-digit", second: "2-digit"};
                     var date = new Date(response["chain"][i]["timestamp"] * 1000);
-                    var formattedDateTime = date.toLocaleTimeString("en-us", options);
+                    var formattedDateTime = $('<div>', {class: "text-end text-muted", text: date.toLocaleTimeString("en-us", options)});
+                    card_text.append(formattedDateTime);
+                } else {
+                    // Append card with index and 'Genesis' title
+                    var block_index = $('<h4>', {text: "Block #" + response['chain'][i]['index']});
+                    var hr = $('<hr>');
+                    var genesis_tag = $('<h3>', {class: "text-center text-secondary", text: "Genesis"})
+                    card_text.append(block_index, hr, genesis_tag);
+                };
+                
 
-                    // Create table row
-                    transaction = [count,
-                        response['chain'][i]['transactions'][j]['recipient'],
-                        response['chain'][i]['transactions'][j]['sender'],
-                        response['chain'][i]['transactions'][j]['amount'],
-                        formattedDateTime,
-                        response['chain'][i]['index']];
+                // Assemble the block and add it to the row
+                card_body.append(card_text);
+                card.append(card_body);
+                block.append(card);
+                row.append(block);
 
-                    transactions.push(transaction);
-                    count += 1;
-                }
-            }
-
-            // Restrict a column to 10 characters, do split words
-            $('#transactions').dataTable({
-                data: transactions,
-                columns: [{title: "#"},
-                        {title: "Recipient Address"},
-                        {title: "Sender Address"},
-                        {title: "Amount"},
-                        {title: "Time"},
-                        {title: "Block"}],
-                columnDefs: [{targets: [1,2,3,4,5], render: $.fn.dataTable.render.ellipsis(25)}]
-            })
+                // Move down a row every four blocks
+                count += 1;
+                if (count % 4 == 0) {
+                    var br = $('<br><br>');
+                    $('#blockchain_visualizer').append(row, br);
+                    row = $('<div>', {class: "row"});
+                };
+            };
+            
+            // Append final row
+            $('#blockchain_visualizer').append(row);
         },
         error: function(error){
             console.log(error);
